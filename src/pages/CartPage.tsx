2179f1b5ebc56c5f8049fa122bfc539a8a31f506
Jordan Needham <jdncreations@hotmail.com>
2025-07-08 14:45:09 +1200

    update cartpage

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,9 +1,87 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+interface CartItem {
+  cartId: number;
+  createdAt: string;
+  id: number;
+  quantity: number;
+  product: {
+    id: number;
+    imageUrl: string;
+    name: string;
+    price: number;
+  };
+}
 
 export default function CartPage() {
-  const [itemsInCart, setItemsInCart] = useState([{ name: 'A.M. Accolade' }]);
+  const [itemsInCart, setItemsInCart] = useState<CartItem[]>([]);
+  const [cartToken, setCartToken] = useState<string | null>(null);
 
-  // get cartId from localStorage, if it exists fetch products from api
+  // get cartToken from localStorage, if it exists fetch products from api
+  useEffect(() => {
+    const fetchCartItems = async () => {
+      console.log('fetching cart items');
+      if (!cartToken) {
+        return;
+      }
+      console.log('fetching response ');
+      const response = await fetch(
+        `http://localhost:3000/api/carts/${cartToken}`
+      );
+      if (!response.ok) {
+        throw new Error('Could not fetch cart items');
+      }
+
+      const data = await response.json();
+      setItemsInCart(data.items);
+      console.log(data);
+    };
+
+    const createCart = async () => {
+      const response = await fetch('http://localhost:3000/api/carts', {
+        method: 'POST',
+      });
+      if (!response.ok) {
+        throw new Error('Could not create new cart');
+      }
+
+      const data = await response.json();
+      if (data.cartToken) {
+        localStorage.setItem('cartToken', data.cartToken);
+        setCartToken(data.cartToken);
+      }
+    };
+
+    const localStorageCartToken = localStorage.getItem('cartToken');
+    if (!localStorageCartToken) {
+      console.log('no token found in localStorage');
+      createCart();
+    } else {
+      setCartToken(localStorageCartToken);
+    }
+
+    fetchCartItems();
+  }, [cartToken]);
+
+  const removeItemFromCart = async (productId: number) => {
+    const body = JSON.stringify({ productId: productId });
+    const response = await fetch(
+      `http://localhost:3000/api/carts/${cartToken}/items`,
+      {
+        method: 'DELETE',
+        body: body,
+      }
+    );
+    if (!response.ok) {
+      throw new Error('Could not remove item from cart');
+    }
+
+    if (response.status === 204) {
+      console.log('success');
+    } else {
+      console.log('uh oh', response.status);
+    }
+  };
 
   return (
     <div className='text-amber-300 '>
@@ -14,8 +92,12 @@ export default function CartPage() {
       <div className='bg-amber-900 rounded rounded-md p-4 my-4'>
         {itemsInCart &&
           itemsInCart.map((item) => (
-            <div>
-              <p className='font-bold'>{item.name}</p>
+            <div key={item.id}>
+              <p className='font-bold'>{item.product.name}</p>
+              <p>{item.quantity}</p>
+              <button onClick={() => removeItemFromCart(item.product.id)}>
+                Remove from cart
+              </button>
             </div>
           ))}
       </div>
